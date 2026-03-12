d69456356deb570c27f83476db6015c313f533c7
divakarhankare9850-ai <divakarhankare9850@gmail.com>
2026-03-13 00:16:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC0LqCpd-u75RfiIPtVUlyT1-cgWE98rLY",
   authDomain: "manas-c8ed8.firebaseapp.com",
@@ -5,4 +8,11 @@ const firebaseConfig = {
   storageBucket: "manas-c8ed8.firebasestorage.app",
   messagingSenderId: "653022231740",
   appId: "1:653022231740:web:4d31e84aa8398107d724b9"
+
 };
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
